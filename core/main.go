14073540f6da20dc8c73be86a0cdd393f1c2165d
Cristian Măgherușan-Stanciu <cristi.magherusan@gmail.com>
2016-11-01 20:25:35 +0100

    Little code cleanup.

--- core/main.go
+++ core/main.go
@@ -24,18 +24,37 @@ func Run(instancesFile string) {
 
 }
 
+// processAllRegions iterates all regions in parallel, and replaces instances
+// for each of the ASGs tagged with 'spot-enabled=true'.
+func processAllRegions(instData *jsonInstances) {
+
+	var wg sync.WaitGroup
+
+	regions, err := getRegions()
+
+	if err != nil {
+		logger.Println(err.Error())
+		return
+	}
+	for _, r := range regions {
+		wg.Add(1)
+		r := region{name: r}
+		go func() {
+			r.processRegion(instData)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+// getRegions generates a list of AWS regions.
 func getRegions() ([]string, error) {
 	var output []string
 
-	// this turns out to be much faster when running locally than using the
-	// commented region auto-detection snipped shown below, and anyway due to
-	// Lambda limitations we currently only support running it from this region.
+	// This turns out to be much faster when running locally than using region
+	// auto-detection, and anyway due to Lambda limitations we currently only
+	// support running it from this region.
 	currentRegion := "us-east-1"
-
-	// m := ec2metadata.New(session.New())
-	// if m.Available() {
-	// 	currentRegion, _ = m.Region()
-	// }
 
 	svc := ec2.New(
 		session.New(
@@ -55,28 +74,4 @@ func getRegions() ([]string, error) {
 		output = append(output, *r.RegionName)
 	}
 	return output, nil
-}
-
-func processAllRegions(instData *jsonInstances) {
-	// for each region in parallel
-	// for each of the ASGs tagged with 'spot-enabled=true'
-
-	var wg sync.WaitGroup
-
-	regions, err := getRegions()
-
-	if err != nil {
-		logger.Println(err.Error())
-		return
-	}
-	for _, r := range regions {
-		wg.Add(1)
-		r := region{name: r}
-		go func() {
-			r.processRegion(instData)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-
 }
