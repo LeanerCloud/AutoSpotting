b053f3b7006be01fbc605be9446e645846ef1110
Artem Nikitin <artemnikitin@artemnikitin.com>
2017-02-06 23:22:51 +0100

    Add test for spot_price.go

--- core/spot_price_test.go
+++ core/spot_price_test.go
@@ -1,6 +1,7 @@
 package autospotting
 
 import (
+	"errors"
 	"math"
 	"os"
 	"testing"
@@ -8,9 +9,20 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
 
 const TOLERANCE = 0.000001
+
+type mock struct {
+	ec2iface.EC2API
+	resp *ec2.DescribeSpotPriceHistoryOutput
+	er   error
+}
+
+func (m mock) DescribeSpotPriceHistory(in *ec2.DescribeSpotPriceHistoryInput) (*ec2.DescribeSpotPriceHistoryOutput, error) {
+	return m.resp, m.er
+}
 
 func TestMain(m *testing.M) {
 	disableLogging()
@@ -120,6 +132,74 @@ func Test_spotPrices_average(t *testing.T) {
 			}
 			if diff := math.Abs(got - tt.want); diff > TOLERANCE {
 				t.Errorf("spotPrices.average() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fetch(t *testing.T) {
+	tests := []struct {
+		name             string
+		config           *spotPrices
+		product          string
+		duration         time.Duration
+		availabilityZone *string
+		instanceTypes    []*string
+		data             []*ec2.SpotPrice
+		err              error
+	}{
+		{
+			name: "error",
+			config: &spotPrices{
+				data: []*ec2.SpotPrice{},
+				conn: connections{
+					ec2: mock{
+						resp: &ec2.DescribeSpotPriceHistoryOutput{
+							SpotPriceHistory: []*ec2.SpotPrice{},
+						},
+						er: errors.New("Error!"),
+					},
+				},
+			},
+			data: []*ec2.SpotPrice{},
+			err:  errors.New("Error!"),
+		},
+		{
+			name: "ok",
+			config: &spotPrices{
+				data: []*ec2.SpotPrice{},
+				conn: connections{
+					ec2: mock{
+						resp: &ec2.DescribeSpotPriceHistoryOutput{
+							SpotPriceHistory: []*ec2.SpotPrice{
+								{SpotPrice: aws.String("1")},
+							},
+						},
+					},
+				},
+			},
+			data: []*ec2.SpotPrice{
+				{SpotPrice: aws.String("1")},
+			},
+			err: errors.New(""),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.config.fetch(tc.product, tc.duration, tc.availabilityZone, tc.instanceTypes)
+			if len(tc.data) != len(tc.config.data) {
+				t.Errorf("Price data actual: %v\nexpected: %v", tc.config.data, tc.data)
+			}
+			if len(tc.data) > 0 {
+				str1 := *tc.data[0].SpotPrice
+				str2 := *tc.config.data[0].SpotPrice
+				if str1 != str2 {
+					t.Errorf("Price actual: %s, expected: %s", str2, str1)
+				}
+			}
+			if err != nil && err.Error() != tc.err.Error() {
+				t.Errorf("error expected: %s, actual: %s", tc.err.Error(), err.Error())
 			}
 		})
 	}
