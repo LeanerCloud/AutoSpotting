8f16228ff25c4aecd27c0106d70f80bffbaa274c
Kevin M. Truckenmiller <ktruckenmiller@gmail.com>
2017-05-31 14:31:11 -0500

    updated deployment

---
+++ local-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## You'll need github.com/ktruckenmiller/docker-friend or an assumed role to do this.
+docker run -it \
+  -v $(pwd):/work \
+  -e deploy_subdirectory=deployment/ \
+  -e environ=dev \
+  -e IAM_ROLE="BDPT2" \
+  056684691971.dkr.ecr.us-east-1.amazonaws.com/bdp/ansible:2.2.0
