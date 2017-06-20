fd6dc45f5fab2f80fee6ec13348ab9835cc3caa7
Sandeep Shantharam <sandeep.shantharam@novartis.com>
2017-06-20 07:33:00 -0400

    Removed PR Request for Log Group

--- terraform/autospotting/lambda.tf
+++ terraform/autospotting/lambda.tf
@@ -47,9 +47,3 @@ resource "aws_cloudwatch_event_rule" "cloudwatch_frequency" {
   name = "autospotting_frequency"
   schedule_expression = "${var.lambda_run_frequency}"
 }
-
-# Commenting out - as reapplying creates an issue when manually creating the log group for Lambda
-# resource "aws_cloudwatch_log_group" "log_group_autospotting" {
-#   name = "/aws/lambda/${aws_lambda_function.autospotting.function_name}"
-#   retention_in_days = 7
-# }
