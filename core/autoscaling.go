61cb5d19e085f9305ed5ff49d08bcb7935b7df06
Cristian Măgherușan-Stanciu <cristi.magherusan@gmail.com>
2017-07-08 15:08:19 +0200

    Cancel spot requests after their instances were terminated

--- core/autoscaling.go
+++ core/autoscaling.go
@@ -476,7 +476,13 @@ func (a *autoScalingGroup) havingReadyToAttachSpotInstance() (*string, bool) {
 	debug.Println(instData)
 
 	if instData == nil || instData.LaunchTime == nil {
-		logger.Println("Apparently", *spotInstanceID, "is no longer running, moving on...")
+		logger.Println("Apparently", *spotInstanceID, "is no longer running, ",
+			"cancelling the spot instance request which created it...")
+
+		a.region.services.ec2.CancelSpotInstanceRequests(
+			&ec2.CancelSpotInstanceRequestsInput{
+				SpotInstanceRequestIds: []*string{activeSpotInstanceRequest.SpotInstanceRequestId},
+			})
 		return nil, true
 	}
 
