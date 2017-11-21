1a5ec6005b553b3e56becbbef6b575ff167627a2
Gaurav Reddy <gaurav.reddy@tabcorp.com.au>
2017-11-21 11:08:00 +1100

    minor fix to refer argument

--- expiration.go
+++ expiration.go
@@ -5,6 +5,6 @@ import "time"
 const dateFormat = "02-Jan-2006"
 
 func isExpired(date string) bool {
-	exp, _ := time.Parse(dateFormat, ExpirationDate)
+	exp, _ := time.Parse(dateFormat, date)
 	return time.Now().After(exp)
 }
