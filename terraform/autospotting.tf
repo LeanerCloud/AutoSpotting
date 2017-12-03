ea25bd0e6df3299ae5b17c3a855f64c5985707cb
Hugo Rosnet <hugo.rosnet@cycloid.io>
2017-12-03 15:49:51 +0100

    terraform: format all files for readiness purposes
    The terraform binary includes an option to format all files
    indentically, thus making the reading/understanding easier.
    
    Simply ran inside terraform/: `terraform fmt -write=true .`

--- terraform/autospotting.tf
+++ terraform/autospotting.tf
@@ -1,14 +1,14 @@
 module "autospotting" {
   source = "./autospotting"
 
-  autospotting_min_on_demand_number = "${var.asg_min_on_demand_number}"
-  autospotting_min_on_demand_percentage = "${var.asg_min_on_demand_percentage}"
+  autospotting_min_on_demand_number       = "${var.asg_min_on_demand_number}"
+  autospotting_min_on_demand_percentage   = "${var.asg_min_on_demand_percentage}"
   autospotting_on_demand_price_multiplier = "${var.asg_on_demand_price_multiplier}"
-  autospotting_regions_enabled = "${var.asg_regions_enabled}"
+  autospotting_regions_enabled            = "${var.asg_regions_enabled}"
 
-  lambda_zipname = "${var.lambda_zipname}"
-  lambda_runtime = "${var.lambda_runtime}"
-  lambda_memory_size = "${var.lambda_memory_size}"
-  lambda_timeout = "${var.lambda_timeout}"
+  lambda_zipname       = "${var.lambda_zipname}"
+  lambda_runtime       = "${var.lambda_runtime}"
+  lambda_memory_size   = "${var.lambda_memory_size}"
+  lambda_timeout       = "${var.lambda_timeout}"
   lambda_run_frequency = "${var.lambda_run_frequency}"
 }
--- terraform/autospotting/lambda.tf
+++ terraform/autospotting/lambda.tf
@@ -1,56 +1,56 @@
 resource "aws_lambda_function" "autospotting" {
-  function_name = "autospotting"
-  filename = "${var.lambda_zipname}"
+  function_name    = "autospotting"
+  filename         = "${var.lambda_zipname}"
   source_code_hash = "${base64sha256(file("${var.lambda_zipname}"))}"
-  role = "${aws_iam_role.autospotting_role.arn}"
-  runtime = "${var.lambda_runtime}"
-  timeout = "${var.lambda_timeout}"
-  handler = "handler.Handle"
-  memory_size = "${var.lambda_memory_size}"
+  role             = "${aws_iam_role.autospotting_role.arn}"
+  runtime          = "${var.lambda_runtime}"
+  timeout          = "${var.lambda_timeout}"
+  handler          = "handler.Handle"
+  memory_size      = "${var.lambda_memory_size}"
 
   environment {
     variables = {
-      MIN_ON_DEMAND_NUMBER = "${var.autospotting_min_on_demand_number}"
-      MIN_ON_DEMAND_PERCENTAGE = "${var.autospotting_min_on_demand_percentage}"
+      MIN_ON_DEMAND_NUMBER       = "${var.autospotting_min_on_demand_number}"
+      MIN_ON_DEMAND_PERCENTAGE   = "${var.autospotting_min_on_demand_percentage}"
       ON_DEMAND_PRICE_MULTIPLIER = "${var.autospotting_on_demand_price_multiplier}"
-      REGIONS = "${var.autospotting_regions_enabled}"
+      REGIONS                    = "${var.autospotting_regions_enabled}"
     }
   }
 }
 
 resource "aws_iam_role" "autospotting_role" {
-  name = "autospotting"
-  path = "/lambda/"
-  assume_role_policy = "${file("${path.module}/lambda-policy.json")}"
+  name                  = "autospotting"
+  path                  = "/lambda/"
+  assume_role_policy    = "${file("${path.module}/lambda-policy.json")}"
   force_detach_policies = true
 }
 
 resource "aws_iam_role_policy" "autospotting_policy" {
-  name = "policy_for_autospotting"
-  role = "${aws_iam_role.autospotting_role.id}"
+  name   = "policy_for_autospotting"
+  role   = "${aws_iam_role.autospotting_role.id}"
   policy = "${file("${path.module}/autospotting-policy.json")}"
 }
 
 resource "aws_lambda_permission" "cloudwatch_events_permission" {
-  statement_id = "AllowExecutionFromCloudWatch"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.autospotting.function_name}"
-  principal = "events.amazonaws.com"
-  source_arn = "${aws_cloudwatch_event_rule.cloudwatch_frequency.arn}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.cloudwatch_frequency.arn}"
 }
 
 resource "aws_cloudwatch_event_target" "cloudwatch_target" {
-  rule = "${aws_cloudwatch_event_rule.cloudwatch_frequency.name}"
+  rule      = "${aws_cloudwatch_event_rule.cloudwatch_frequency.name}"
   target_id = "run_autospotting"
-  arn = "${aws_lambda_function.autospotting.arn}"
+  arn       = "${aws_lambda_function.autospotting.arn}"
 }
 
 resource "aws_cloudwatch_event_rule" "cloudwatch_frequency" {
-  name = "autospotting_frequency"
+  name                = "autospotting_frequency"
   schedule_expression = "${var.lambda_run_frequency}"
 }
 
 resource "aws_cloudwatch_log_group" "log_group_autospotting" {
-  name = "/aws/lambda/${aws_lambda_function.autospotting.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.autospotting.function_name}"
   retention_in_days = 7
 }
