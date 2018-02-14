505f3a688328203238088e1244de213a6197ba2c
Javeria Khan <jkhan@branch.io>
2018-02-14 11:29:10 -0800

    Remove isSpotQuantityCompatible test

--- core/instance_test.go
+++ core/instance_test.go
@@ -712,18 +712,6 @@ func TestIsQuantityCompatible(t *testing.T) {
 			expected:           true,
 		},
 	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			a := &autoScalingGroup{
-				name:      tt.name,
-				instances: tt.asgInstances,
-				Group: &autoscaling.Group{
-					DesiredCapacity: tt.asgDesiredCapacity,
-				},
-			}
-		})
-	}
 }
 
 func TestGetCheapestCompatibleSpotInstanceType(t *testing.T) {
