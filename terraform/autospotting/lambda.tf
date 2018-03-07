e969d7300cc49e2a9ec3c4ebf7e2c252705437b6
grahamlyons <graham@grahamlyons.com>
2018-03-07 13:39:43 +0000

    Format the Terraform code using `terraform fmt`

--- terraform/autospotting/lambda.tf
+++ terraform/autospotting/lambda.tf
@@ -1,27 +1,26 @@
 module "aws_lambda_function" {
   source = "./lambda"
 
-  lambda_zipname     = "${var.lambda_zipname}"
+  lambda_zipname = "${var.lambda_zipname}"
 
-  lambda_s3_bucket   = "${var.lambda_s3_bucket}"
-  lambda_s3_key      = "${var.lambda_s3_key}"
+  lambda_s3_bucket = "${var.lambda_s3_bucket}"
+  lambda_s3_key    = "${var.lambda_s3_key}"
 
   lambda_role_arn    = "${aws_iam_role.autospotting_role.arn}"
   lambda_runtime     = "${var.lambda_runtime}"
   lambda_timeout     = "${var.lambda_timeout}"
   lambda_memory_size = "${var.lambda_memory_size}"
-  
-  
-  autospotting_allowed_instance_types = "${var.autospotting_allowed_instance_types}"
-  autospotting_disallowed_instance_types = "${var.autospotting_disallowed_instance_types}"
-  autospotting_min_on_demand_number = "${var.autospotting_min_on_demand_number}"
-  autospotting_min_on_demand_percentage = "${var.autospotting_min_on_demand_percentage}"
-  autospotting_on_demand_price_multiplier = "${var.autospotting_on_demand_price_multiplier}"
+
+  autospotting_allowed_instance_types       = "${var.autospotting_allowed_instance_types}"
+  autospotting_disallowed_instance_types    = "${var.autospotting_disallowed_instance_types}"
+  autospotting_min_on_demand_number         = "${var.autospotting_min_on_demand_number}"
+  autospotting_min_on_demand_percentage     = "${var.autospotting_min_on_demand_percentage}"
+  autospotting_on_demand_price_multiplier   = "${var.autospotting_on_demand_price_multiplier}"
   autospotting_spot_price_buffer_percentage = "${var.autospotting_spot_price_buffer_percentage}"
-  autospotting_spot_product_description = "${var.autospotting_spot_product_description}"
-  autospotting_bidding_policy = "${var.autospotting_bidding_policy}"
-  autospotting_regions_enabled = "${var.autospotting_regions_enabled}"
-  autospotting_tag_filters = "${var.autospotting_tag_filters}"
+  autospotting_spot_product_description     = "${var.autospotting_spot_product_description}"
+  autospotting_bidding_policy               = "${var.autospotting_bidding_policy}"
+  autospotting_regions_enabled              = "${var.autospotting_regions_enabled}"
+  autospotting_tag_filters                  = "${var.autospotting_tag_filters}"
 }
 
 resource "aws_iam_role" "autospotting_role" {
