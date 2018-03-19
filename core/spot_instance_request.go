5ce54bdb3878c62b1612b9cfff59548560a1a23c
dominictootell <dominic.tootell@gmail.com>
2018-03-19 20:45:30 +0000

    fix tagging instance loop error

--- core/spot_instance_request.go
+++ core/spot_instance_request.go
@@ -19,56 +19,6 @@ type spotInstanceRequest struct {
 	asg    *autoScalingGroup
 }
 
-// This function returns an Instance ID
-func (s *spotInstanceRequest) waitForAndTagSpotInstance() error {
-	logger.Println(s.asg.name, "Waiting for spot instance for spot instance request",
-		*s.SpotInstanceRequestId)
-
-	ec2Client := s.region.services.ec2
-
-	params := ec2.DescribeSpotInstanceRequestsInput{
-		SpotInstanceRequestIds: []*string{s.SpotInstanceRequestId},
-	}
-
-	err := ec2Client.WaitUntilSpotInstanceRequestFulfilled(&params)
-	if err != nil {
-		logger.Println(s.asg.name, "Error waiting for instance:", err.Error())
-		return err
-	}
-
-	logger.Println(s.asg.name, "Done waiting for an instance.")
-
-	// Now we try to get the InstanceID of the instance we got
-	requestDetails, err := ec2Client.DescribeSpotInstanceRequests(&params)
-	if err != nil {
-		logger.Println(s.asg.name, "Failed to describe spot instance requests")
-		return err
-	}
-
-	// due to the waiter we can now safely assume all this data is available
-	spotInstanceID := requestDetails.SpotInstanceRequests[0].InstanceId
-
-	logger.Println(s.asg.name, "Found new spot instance", *spotInstanceID)
-	logger.Println("Tagging it to match the other instances from the group")
-
-	// we need to re-scan in order to have the information a
-	err = s.region.scanInstances()
-	if err != nil {
-		logger.Printf("Failed to scan instances: %s for %s\n", err, s.asg.name)
-	}
-
-	tags := s.asg.propagatedInstanceTags()
-
-	i := s.region.instances.get(*spotInstanceID)
-
-	if i != nil {
-		i.tag(tags, defaultTimeout)
-	} else {
-		logger.Println(s.asg.name, "new spot instance", *spotInstanceID, "has disappeared")
-	}
-	return nil
-}
-
 func (s *spotInstanceRequest) tag(asgName string) error {
 	svc := s.region.services.ec2
 	params := ec2.CreateTagsInput{
@@ -85,12 +35,12 @@ func (s *spotInstanceRequest) tag(asgName string) error {
 		},
 	}
 
-	for count := 0; count < 10; count++ {
+	for count := 1; count < 11; count++ {
 		_, err := svc.CreateTags(&params)
 
 		if err != nil {
-			if count > 10 {
-				logger.Println("Failed to mark the spot instance request as complete after 10 retries:",
+			if count == 10 {
+				logger.Println("Failed to mark the spot instance request as complete after 10 tries:",
 					"cancelling the spot instance request, error: ", err.Error())
 
 				s.reload()
