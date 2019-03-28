23ef6b6c3f4509bc943de6006d2177fc4c0e0103
Cristian Măgherușan-Stanciu <cristi.magherusan@gmail.com>
2019-03-28 10:29:54 +0100

    Fix CodeClimate and pylint issues

--- cloudformation/stacks/AutoSpotting/regional_stack_lambda.py
+++ cloudformation/stacks/AutoSpotting/regional_stack_lambda.py
@@ -1,76 +1,75 @@
-import boto3
-import botocore
-import json
-import sys
+'''
+    Creates regional CloudFormation stacks that trigger the main AutoSpotting
+    Lambda function
+'''
+
 import traceback
 
+from json import dumps
+from sys import exc_info
 from threading import Thread
+
+from boto3 import client
+from botocore.exceptions import ClientError
 from botocore.vendored import requests
+from botocore.vendored.requests.exceptions import RequestException
 
 SUCCESS = "SUCCESS"
 FAILED = "FAILED"
 STACK_NAME = 'AutoSpottingRegionalResources'
-TEMPLATE_URL = 'https://s3.amazonaws.com/cloudprowess/nightly/regional_template.yaml'
+TEMPLATE_URL = \
+    'https://s3.amazonaws.com/cloudprowess/nightly/regional_template.yaml'
 
 
 def create_stack(region, lambda_arn):
-    client = boto3.client('cloudformation', region)
+    '''Creates a regional CloudFormation stack'''
+    cfn = client('cloudformation', region)
     response = {}
 
     # delete stack if it already exists from a previous run
     try:
-        response = client.delete_stack(StackName=STACK_NAME)
-        print(response)
-        waiter = client.get_waiter('stack_delete_complete')
-        waiter.wait(
-            StackName=STACK_NAME,
-            WaiterConfig={'Delay': 5}
-        )
-    except botocore.exceptions.ClientError:
+        delete_stack(region)
+    except ClientError:
         pass
 
-    try:
-        response = client.create_stack(
-            StackName=STACK_NAME,
-            TemplateURL=TEMPLATE_URL,
-            Capabilities=['CAPABILITY_IAM'],
-            Parameters=[
-                {
-                    'ParameterKey': 'AutoSpottingLambdaARN',
-                    'ParameterValue': lambda_arn,
-                },
-            ],
-        )
-        print(response)
-    except botocore.exceptions.ClientError:
-        raise
+    response = cfn.create_stack(
+        StackName=STACK_NAME,
+        TemplateURL=TEMPLATE_URL,
+        Capabilities=['CAPABILITY_IAM'],
+        Parameters=[
+            {
+                'ParameterKey': 'AutoSpottingLambdaARN',
+                'ParameterValue': lambda_arn,
+            },
+        ],
+    )
+    print(response)
 
 
 def delete_stack(region):
-    client = boto3.client('cloudformation', region)
+    ''' Deletes a regional CloudFormation stack'''
+    cfn = client('cloudformation', region)
 
-    try:
-        response = client.delete_stack(StackName=STACK_NAME)
-        print(response)
+    response = cfn.delete_stack(StackName=STACK_NAME)
+    print(response)
 
-        waiter = client.get_waiter('stack_delete_complete')
-        waiter.wait(
-            StackName=STACK_NAME,
-            WaiterConfig={'Delay': 5}
-        )
-    except botocore.exceptions.ClientError:
-        raise
+    waiter = cfn.get_waiter('stack_delete_complete')
+    waiter.wait(
+        StackName=STACK_NAME,
+        WaiterConfig={'Delay': 5}
+    )
 
 
 def handle_create(event):
-    ec2 = boto3.client('ec2')
+    ''' Creates regional stacks in all available AWS regions concurrently '''
+    ec2 = client('ec2')
     lambda_arn = event['ResourceProperties']['LambdaARN']
     threads = []
 
     # create concurrently in all regions
     for region in ec2.describe_regions()['Regions']:
-        process = Thread(target=create_stack, args=[
-                         region['RegionName'], lambda_arn])
+        process = Thread(target=create_stack,
+                         args=[region['RegionName'], lambda_arn])
         process.start()
         threads.append(process)
 
@@ -78,8 +77,9 @@ def handle_create(event):
         process.join()
 
 
-def handle_delete(event):
-    ec2 = boto3.client('ec2')
+def handle_delete():
+    ''' Concurrently deletes regional stacks in all available AWS regions '''
+    ec2 = client('ec2')
     threads = []
 
     # delete concurrently in all regions
@@ -93,52 +93,49 @@ def handle_delete(event):
 
 
 def handler(event, context):
+    ''' Lambda function entry point '''
     try:
-        if event['RequestType'] == 'Delete':
-            handle_delete(event)
-
         if event['RequestType'] == 'Create':
             handle_create(event)
-
+        if event['RequestType'] == 'Delete':
+            handle_delete()
         send(event, context, SUCCESS, {})
-    except:
+    except ClientError:
         traceback.print_exc()
-        print("Unexpected error:", sys.exc_info()[0])
+        print("Unexpected error:", exc_info()[0])
         send(event, context, FAILED, {})
 
-# Informs CloudFormation about the state of the custom resource
 
+def send(event, context, response_status, response_data):
+    ''' Informs CloudFormation about the state of the custom resource '''
+    response_url = event['response_url']
 
-def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False):
-    responseUrl = event['ResponseURL']
+    print(response_url)
 
-    print(responseUrl)
+    response_body = {}
+    response_body['Status'] = response_status
+    response_body['Reason'] = \
+        'See the details in CloudWatch Log Stream: ' + context.log_stream_name
+    response_body['PhysicalResourceId'] = context.log_stream_name
+    response_body['StackId'] = event['StackId']
+    response_body['RequestId'] = event['RequestId']
+    response_body['LogicalResourceId'] = event['LogicalResourceId']
+    response_body['NoEcho'] = None
+    response_body['Data'] = response_data
 
-    responseBody = {}
-    responseBody['Status'] = responseStatus
-    responseBody[
-        'Reason'] = 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
-    responseBody[
-        'PhysicalResourceId'] = physicalResourceId or context.log_stream_name
-    responseBody['StackId'] = event['StackId']
-    responseBody['RequestId'] = event['RequestId']
-    responseBody['LogicalResourceId'] = event['LogicalResourceId']
-    responseBody['NoEcho'] = noEcho
-    responseBody['Data'] = responseData
+    json_response_body = dumps(response_body)
 
-    json_responseBody = json.dumps(responseBody)
-
-    print("Response body:\n" + json_responseBody)
+    print("Response body:\n" + json_response_body)
 
     headers = {
         'content-type': '',
-        'content-length': str(len(json_responseBody))
+        'content-length': str(len(json_response_body))
     }
 
     try:
-        response = requests.put(responseUrl,
-                                data=json_responseBody,
+        response = requests.put(response_url,
+                                data=json_response_body,
                                 headers=headers)
         print("Status code: " + response.reason)
-    except Exception as e:
-        print("send(..) failed executing requests.put(..): " + str(e))
+    except RequestException as exception:
+        print("send(..) failed executing requests.put(..): " + str(exception))
