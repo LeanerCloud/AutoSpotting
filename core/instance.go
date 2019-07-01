9c5a7118b361f0e1f2768b07811ec032ff0817b9
Cristian Măgherușan-Stanciu <cristian.magherusan-stanciu@here.com>
2019-07-01 11:31:27 +0200

    Fix code formatting

--- core/instance.go
+++ core/instance.go
@@ -514,8 +514,8 @@ func (i *instance) launchTemplateHasNetworkInterfaces(ver, id *string) bool {
 	)
 
 	if err != nil {
-    logger.Println("Failed to describe launch template", *id, "version", *ver,
-    "encountered error:", err.Error())
+		logger.Println("Failed to describe launch template", *id, "version", *ver,
+			"encountered error:", err.Error())
 	}
 
 	if err == nil && len(res.LaunchTemplateVersions) == 1 {
