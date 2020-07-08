3f849fbd1c71bfa8f43b140a0eabf8ac842c7310
mello7tre <mello+github@ankot.org>
2020-07-08 16:19:24 +0200

    Merge Delete DescribeSecurityGroups mock [3bfd542]

--- core/mock_test.go
+++ core/mock_test.go
@@ -5,10 +5,8 @@ package autospotting
 
 import (
 	"reflect"
-	"strings"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/autoscaling/autoscalingiface"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
@@ -101,46 +99,6 @@ func (m mockEC2) DescribeLaunchTemplateVersions(*ec2.DescribeLaunchTemplateVersi
 
 func (m mockEC2) WaitUntilInstanceRunning(*ec2.DescribeInstancesInput) error {
 	return m.wuirerr
-}
-
-// For testing we "convert" the SecurityGroupIDs/SecurityGroupNames by
-// prefixing the original name/id with "sg-" if not present already. We
-// also fill up the rest of the string to the length of a typical ID with
-// characters taken from the string "deadbeef"
-func (m mockEC2) DescribeSecurityGroups(input *ec2.DescribeSecurityGroupsInput) (*ec2.DescribeSecurityGroupsOutput, error) {
-	var groups []*ec2.SecurityGroup
-
-	// we use this string to fill the length of an SecurityGroup name to an
-	// ID if the name is too short to be a correct ID
-	const testFillStringID = "deadbeef"
-
-	// "sg-" + 8 hex characters
-	const testLengthIDString = 11
-
-	for _, groupName := range input.GroupNames {
-		newgroup := *groupName
-
-		if !strings.HasPrefix(*groupName, "sg-") {
-			newgroup = "sg-" + *groupName
-		}
-
-		// a SecurityGroupID is supposed to have a length of 11
-		// characters. We fill up the missing characters to indicate that this is
-		// now an ID and that it was treated as a name before
-		lenng := len(newgroup)
-		if lenng < testLengthIDString {
-			needed := testLengthIDString - lenng
-			newgroup = newgroup + testFillStringID[:needed]
-		}
-
-		groups = append(groups, &ec2.SecurityGroup{GroupId: &newgroup})
-	}
-
-	for _, groupID := range input.GroupIds {
-		groups = append(groups, &ec2.SecurityGroup{GroupId: aws.String(*groupID)})
-	}
-
-	return &ec2.DescribeSecurityGroupsOutput{SecurityGroups: groups}, nil
 }
 
 // All fields are composed of the abbreviation of their method
