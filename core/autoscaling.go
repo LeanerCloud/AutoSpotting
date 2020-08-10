557a0fb0df25f6fa3fb4819eed9a8d624181e8e7
mello7tre <mello+github@ankot.org>
2020-08-10 16:45:50 +0200

    Avoid adding instance in Terminating Lifecycle State
    Problem arise if ASG have a LifeCycle Hook on terminating instances and
    the lifecycle action take more time than the Autospotting schedule.
    
    When terminating instances LifeCycle Hook (LH) put the instance in the
    lifecycle state Terminating:Wait (and later Terminating:Proceed).
    But during that time, until LH complete the instance status is still running.
    
    So AutoSpotting when counting running instances will consider that
    instance too.
    
    This will cause multiples problems:
    * spot instance can be terminated even if needed
    * instance can be terminated even if already in terminating state
    * spot instance are launched even if not needed
    
    The fix is very simple:
    in "scanInstances" func do not execute "a.instances.add" if the instance
    lifecycle begin with Terminating.

--- core/autoscaling.go
+++ core/autoscaling.go
@@ -308,6 +308,11 @@ func (a *autoScalingGroup) scanInstances() instances {
 			i.price = i.typeInfo.pricing.onDemand + i.typeInfo.pricing.premium
 		}
 
+		// Avoid adding instance in Terminating (Wait|Proceed) Lifecycle State
+		if strings.HasPrefix(*inst.LifecycleState, "Terminating"){
+			continue
+		}
+
 		a.instances.add(i)
 	}
 	return a.instances
