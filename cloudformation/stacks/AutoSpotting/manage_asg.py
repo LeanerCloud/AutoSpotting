7a7f45403803b5a24a522e863ba0134e28d7ae60
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2020-11-13 12:07:07 +0100

    Address codeclimate issues of manage_asg.py

--- cloudformation/stacks/AutoSpotting/manage_asg.py
+++ cloudformation/stacks/AutoSpotting/manage_asg.py
@@ -8,33 +8,86 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def change_max_size(svc, region, asg, variation):
+def change_max_size(svc, asg, variation):
     try:
         response = svc.describe_auto_scaling_groups(
-            AutoScalingGroupNames=[asg],
+            ASGNames=[asg],
         )
 
-        if not response['AutoScalingGroups']:
-            raise Exception(f'AutoScalingGroup {asg} not found!')
+        if not response['ASGs']:
+            raise Exception(f'ASG {asg} not found!')
     except Exception as e:
         logger.error(f'Failed to describe {asg}: {e}')
         return False
 
-    currentSize = response['AutoScalingGroups'][0]['MaxSize']
+    currentSize = response['ASGs'][0]['MaxSize']
     newSize = int(currentSize) + int(variation)
 
     try:
         response = svc.update_auto_scaling_group(
-            AutoScalingGroupName=asg,
+            ASGName=asg,
             MaxSize=newSize,
         )
     except Exception as e:
         logger.error(
-            f'Failed to change AutoScalingGroup {asg} MaxSize to {newSize}: {e}')
+            f'Failed to change ASG {asg} MaxSize to {newSize}: {e}')
         return False
 
-    logger.info(f'AutoScalingGroup {asg} MaxSize changed to {newSize}')
+    logger.info(f'ASG {asg} MaxSize changed to {newSize}')
     return True
+
+
+def suspend(svc, asg, tag):
+    try:
+        svc.suspend_processes(
+            ASGName=asg,
+            ScalingProcesses=['Terminate'],
+        )
+        return True
+    except Exception as e:
+        logger.error(
+            f'Failed suspend process on ASG {asg}: {e}')
+        return False
+    else:
+        try:
+            svc.create_or_update_tags(Tags=[tag])
+            return True
+        except Exception as e:
+            logger.error(
+                f'Failed to tag ASG {asg} for suspend ' +
+                'process by {instanceId}: {e}')
+            return False
+
+
+def resume(svc, asg, tag, instanceId):
+    try:
+        response = svc.describe_tags(
+            Filters=[
+                {
+                    'Name': 'auto-scaling-group',
+                    'Values': [asg],
+                },
+                {
+                    'Name': 'key',
+                    'Values': ['autospotting_suspend_processes_by'],
+                },
+                {
+                    'Name': 'value',
+                    'Values': [instanceId],
+                }
+            ]
+        )
+        if response['Tags']:
+            svc.resume_processes(
+                ASGName=asg,
+                ScalingProcesses=['Terminate'],
+            )
+            svc.delete_tags(Tags=[tag])
+            return True
+    except Exception as e:
+        logger.error(
+            f'Failed resume process on ASG {asg}: {e}')
+        return False
 
 
 def suspend_resume(svc, region, asg, action, instanceId):
@@ -46,61 +99,20 @@ def suspend_resume(svc, region, asg, action, instanceId):
         'Value': instanceId,
     }
     if action == 'suspend':
-        try:
-            svc.suspend_processes(
-                AutoScalingGroupName=asg,
-                ScalingProcesses=['Terminate'],
-            )
-        except Exception as e:
-            logger.error(
-                f'Failed suspend process on AutoScalingGroup {asg}: {e}')
-            return False
-        else:
-            try:
-                svc.create_or_update_tags(Tags=[tag])
-                return True
-            except Exception as e:
-                logger.error(
-                    f'Failed to tag AutoScalingGroup {asg} for suspend process by {instanceId}: {e}')
-                return False
-    if action == 'resume':
-        try:
-            response = svc.describe_tags(
-                Filters=[
-                    {
-                        'Name': 'auto-scaling-group',
-                        'Values': [asg],
-                    },
-                    {
-                        'Name': 'key',
-                        'Values': ['autospotting_suspend_processes_by'],
-                    },
-                    {
-                        'Name': 'value',
-                        'Values': [instanceId],
-                    }
-                ]
-            )
-            if response['Tags']:
-                svc.resume_processes(
-                    AutoScalingGroupName=asg,
-                    ScalingProcesses=['Terminate'],
-                )
-                svc.delete_tags(Tags=[tag])
-        except Exception as e:
-            logger.error(
-                f'Failed resume process on AutoScalingGroup {asg}: {e}')
-            return False
+        return suspend(svc, asg, tag)
+    elif action == 'resume':
+        return resume(svc, asg, tag, instanceId)
 
 
 def handler(event, context):
     region = event['region']
-    svc = client('autoscaling', region_name=event['region'])
+    svc = client('autoscaling', region_name=region)
     asg = event['asg']
+
     if 'variation' in event:
         variation = event['variation']
-        return change_max_size(svc, region, asg, variation)
+        return change_max_size(svc, asg, variation)
     else:
         instanceId = event['instanceid']
         action = event['action']
-        return suspend_resume(svc, region, asg, action, instanceId)
+        return suspend_resume(svc, asg, action, instance
