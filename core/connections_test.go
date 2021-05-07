1f2e25bc43a3fae807872680d6fc81830ba5352e
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2021-05-07 12:28:50 +0200

    Fix tests

--- core/connections_test.go
+++ core/connections_test.go
@@ -23,7 +23,7 @@ func Test_connections_connect(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &connections{}
-			c.connect(tt.region)
+			c.connect(tt.region, "bar")
 			if (c.region == tt.region) != tt.match {
 				t.Errorf("connections.connect() c.region = %v, expected %v",
 					c.region, tt.region)
